d24e27fff06ebd9def2ea0646af87c796795450b
Elbouhmydy Abdelmoughit <elbouhmydyabdelmoughit@gmail.com>
2023-02-08 21:14:51 +0000

    finished model User

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -4,21 +4,22 @@ import {
   pre,
   prop,
   Severity,
+  DocumentType
 } from "@typegoose/typegoose";
 import { nanoid } from "nanoid";
 import argon2 from "argon2";
+import log from "../utils/logger";
 
 @pre<User>("save", async function () {
   if (!this.isModified("password")) {
     return;
   }
+  const hash = await argon2.hash(this.password);
 
-  const hash = argon2.hash(this.password);
-  this.password = hash
+  this.password = hash;
+
   return;
-  
 })
-
 @modelOptions({
   schemaOptions: {
     timestamps: true,
@@ -48,6 +49,16 @@ export class User {
 
   @prop({ default: false })
   verified: boolean;
+
+  async validatePassword(this: DocumentType<User>, candidatePassword: string) {
+    try {
+      return await argon2.verify(this.password, candidatePassword);
+    } catch (e) {
+      log.error(e, "Could not validate password");
+      return false;
+    }
+  }
+
 }
 
 const UserModel = getModelForClass(User);
